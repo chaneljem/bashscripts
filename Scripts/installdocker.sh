f28b0818485e050f441f525414b3d16482023aec
chaneljem <chanel.jemmott@gmail.com>
2023-03-11 09:36:21 -0500

    install docker

---
+++ Scripts/installdocker.sh
@@ -0,0 +1,12 @@
+#Install Docker on Ubuntu
+#!/bin/bash
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-install docker.io -y
+sudo snap install docker
+docker --version
+sudo sleep 5
+sudo dockerrun hello-world
+sudo docker images
+sudo docker ps -a
+sudo docker image ls
